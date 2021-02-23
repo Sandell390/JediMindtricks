a290b7eefef4eae788945008730aae2dfd6c5e24
Jesper Sandell <sandell327@gmail.com>
2021-02-23 08:06:38 +0100

    Add project files.

---
+++ Jedi Mind tricks/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jedi_Mind_tricks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Opg 1
+            Dictionary<string, int> mig = new Dictionary<string, int>(); 
+
+            //Opg 2
+            mig.Add("Jesper", 19);
+
+            mig["Han"] = 59;
+            mig["Hun"] = 49;
+
+            //Opg 3
+            mig.Remove("Han");
+
+            //Opg 4
+            foreach (var pair in mig)
+            {
+                Console.WriteLine($"Found: {pair.Value}, with key: {pair.Key}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
